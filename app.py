a14aaa6b3f5b8f3cc03e28466de0817bf5f70a7e
bilal-younus <bilalyounus33@gmail.com>
2022-06-24 01:13:32 +0500

    all the files push

---
+++ app.py
@@ -0,0 +1,46 @@
+
+# Importing essential libraries
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
+import os
+import numpy as np
+import nltk
+
+# Load the Random Forest CLassifier model
+file = os.path.abspath("D:/semester 7/machine learning/project/emotion/EMOTION PREDICTION/nlp.pkl")
+filename = 'nlp.pkl'
+classifier = pickle.load(open(file, 'rb'))
+cv = pickle.load(open(file, 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    if request.method == 'POST':
+        return
+    # model.save()
+    # Failure to return a redirect or render_template
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    errors=[]
+    if request.method == 'POST':
+        try:
+            message = request.form['text']
+            data = [message]
+            vect = cv.transform(data).toarray()
+            prediction = classifier.predict(vect)
+            string = " "
+            emotion = string.join(prediction)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('home.html', prediction=emotion, errors=errors)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
